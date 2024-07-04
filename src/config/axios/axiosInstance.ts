6df7941c3800eda11b50ef9661b6d2275583c741
hunterpvl <hunterpvl@gmail.com>
2024-07-04 22:44:08 +0700

    Update axiosInstance.ts

--- src/config/axios/axiosInstance.ts
+++ src/config/axios/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios
 let axiosInstance = axios.create({
-  baseURL: "http://localhost:9001/api/v1/", // Your backend URL
+  baseURL: "http://91.108.104.57/api/v1/", // Your backend URL
   headers: {
     "Content-Type": "application/json",
   },
