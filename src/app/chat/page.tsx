54214ee0a110c6845fe8f9e490224f4db2d8130b
Nguyễn Văn Rin <Rinchan98.py@gmail.com>
2024-11-07 16:02:17 +0700

    add husky and eslint

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -7,8 +7,6 @@ import {
   dehydrate,
   HydrationBoundary,
 } from '@tanstack/react-query';
-import { FetchContentMessage } from "@/hooks/getContentMessage";
-import { FetchConversation } from "@/hooks/conversation";
 
 export default function ChatPage() {
   const layout = cookies().get("react-resizable-panels:layout");
@@ -17,22 +15,6 @@ export default function ChatPage() {
   const defaultLayout = layout ? JSON.parse(layout?.value) : undefined;
   const defaultCollapsed =
     collapsed && collapsed.value ? JSON.parse(collapsed.value) : undefined;
-
-    const queryClient = getQueryClient()
-    // void queryClient.prefetchQuery({
-    //   queryKey: ['user'],
-    //   queryFn: () => FetchCurrentUser(),
-    // })
-
-    // void queryClient.prefetchQuery({
-    //   queryKey: ['contentConversationId', 1],
-    //   queryFn: () => FetchContentMessage(1),
-    // })
-
-    // void queryClient.prefetchQuery({
-    //   queryKey: ['conversation'],
-    //   queryFn: () => FetchConversation(),
-    // })
 
   return (
     <div className="hidden z-10 min-w-full w-full h-full flex-col md:flex">
